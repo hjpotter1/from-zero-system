f2e249ad21c42ec77417e2c852227c6380c936f7
hjpotter1 <hjpotter2@gmail.com>
2021-05-23 01:41:12 +0900

    1111

---
+++ back-end/run.py
@@ -0,0 +1,39 @@
+from flask import Flask, request
+import os
+import json
+import SqliteUtil as DBUtil
+
+app = Flask(__name__, template_folder='../front-end',
+            static_folder='../front-end', static_url_path='')
+
+
+@app.route('/hi')
+def hi():
+    return 'hi~'
+
+
+# api接口前缀
+apiPrefix = '/api/v1/'
+
+##################  Staff接口  ##################
+
+
+@app.route(apiPrefix + 'updateStaff', methods=['POST'])
+def updateStaff():
+    data = request.get_data(as_text=True)
+    print("后端数据：", data)
+    re = DBUtil.addOrUpdateStaff(data)
+    return json.dumps(re)
+
+
+@app.route(apiPrefix + 'getStaffList/<int:job>')
+def getStaffList(job):
+    # [('1', '1', '1', '1', '1'), ('1', '1', '2', '3', '4'), ...] 二维数组
+    array = DBUtil.getStaffList(job)
+    jsonStaffs = DBUtil.getStaffsFromData(array)
+    print("jsonStaffs:", jsonStaffs)
+    return json.dumps(jsonStaffs)
+
+
+if __name__ == "__main__":
+    app.run(debug=True, port=5001)
---
+++ front-end/src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import HomePage from './pages/HomePage'
+
+import 'antd/dist/antd.css'
+import './App.css'
+
+class App extends Component {
+  render() {
+    return <HomePage />
+  }
+}
+
+export default App
